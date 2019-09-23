097a548ae074647db0b2733a20275e142556079a
LeftBased <55230643+LeftBased@users.noreply.github.com>
2019-09-23 16:57:49 -0700

    Add files via upload
    v0.02

--- BL_Weapon_DPS_Calculator.js
+++ BL_Weapon_DPS_Calculator.js
@@ -1,60 +1,88 @@
 // Borderlands DPS Calculator - JScript edition!
-// v0.01
+//============================================================================
+// [changelog]
+// v0.02 - added an error alert function
+//       - updated safety checks
+//       - fixed an accidental bug in code.
+// v0.01 - initial start of project totally works
+//============================================================================
 var CanCalc = 0; //to prevent errors while trying to calculate invalid inputs.
-
-var MagSize = 0; var FireRate = 0.00; var ReloadSpeed = 0.00; var WeaponDamage = 0;
+var MagSize; var FireRate; var ReloadSpeed; var WeaponDamage;
 var secMF = 0; var secMFR = 0; var TotalMagDamage = 0; var FinalDPS;
 
-function GetWeaponDamage() {
-	while (isNaN(parseFloat(WeaponDamage = prompt("Enter Weapon Damage", "42"))));
-	SafetyCheck(); //runs this to make sure stuff is valid!
-}
-function GetMagazineSize() {
-	while (isNaN(parseFloat(MagSize = prompt("Enter Weapon Magazine Size", "18"))));
-	SafetyCheck(); //runs this to make sure stuff is valid!
-}
-function GetReloadSpeed() {
-	while (isNaN(parseFloat(ReloadSpeed = prompt("Enter Weapon Reload Speed", "1.5"))));
-	SafetyCheck(); //runs this to make sure stuff is valid!
-}
-function GetFireRate() {
-	while (isNaN(parseFloat(FireRate = prompt("Enter Weapon FireRate", "10.55"))));
-	SafetyCheck(); //runs this to make sure stuff is valid!
+function ErrAlert(eCode){
+	switch(eCode){
+		case 0:
+		alert("[Error 0] - Weapon Damage is Less than 0");
+		break;
+		case 1:
+		alert("[Error 1] - Magazine Size is Less than 0");
+		break;
+		case 2:
+		alert("[Error 2] - ReloadSpeed is Less than 0");
+		break;
+		case 3:
+		alert("[Error 3] - FireRate is Less than 0");
+		break;
+	}
 }
 
-function SafetyCheck() {
-	if (parseFloat(WeaponDamage < 1)) {
-		CanCalc = 0;
-		console.log("WeaponDamage is less than 1");
-		GetWeaponDamage();
-	}
-	else {
-		CanCalc = 1;
-	}
-	if (parseFloat(MagSize < 1)) {
-		CanCalc = 0;
-		console.log("MagSize is less than 1");
-		GetMagazineSize();
-	}
-	else {
-		CanCalc = 1;
-	}
-	if (parseFloat(ReloadSpeed < 1)) {
-		CanCalc = 0;
-		console.log("ReloadSpeed is less than 1");
-		GetReloadSpeed();
-	}
-	else {
-		CanCalc = 1;
-	}
-	if (parseFloat(FireRate < 1)) {
-		CanCalc = 0;
-		console.log("Firerate is less than 1");
-		GetFireRate();
-	}
-	else {
-		CanCalc = 1;
-	}
+function GetWeaponDamage() {
+	while (isNaN(WeaponDamage = prompt("Enter Weapon Damage", "42")));
+	SafetyCheck(0);
+}
+function GetMagazineSize() {
+	while (isNaN(MagSize = prompt("Enter Weapon Magazine Size", "18")));
+	SafetyCheck(1);
+}
+function GetReloadSpeed() {
+	while (isNaN(ReloadSpeed = prompt("Enter Weapon Reload Speed", "1.5")));
+	SafetyCheck(2);
+}
+function GetFireRate() {
+	while (isNaN(FireRate = prompt("Enter Weapon FireRate", "10.55")));
+	SafetyCheck(3);
+}
+
+function SafetyCheck(sCode) {
+	switch(sCode){
+		case 0:
+			if (WeaponDamage < 1) {
+				CanCalc = 0;
+				ErrAlert(0);
+				GetWeaponDamage();
+			} else {
+				CanCalc = 1;
+			}
+			break;
+	    case 1:
+			if (MagSize < 1) {
+				CanCalc = 0;
+				ErrAlert(1);
+				GetMagazineSize();
+			} else {
+				CanCalc = 1;
+			}
+			break;
+		case 2:
+			if (ReloadSpeed < 1) {
+				CanCalc = 0;
+				ErrAlert(2);
+				GetReloadSpeed();
+			} else {
+				CanCalc = 1;
+			}
+			break;
+		case 3:
+			if (FireRate < 1) {
+				CanCalc = 0;
+				ErrAlert(3);
+				GetFireRate();
+			} else {
+				CanCalc = 1;
+			}
+			break;
+		}
 }
 
 function GrabWeaponData() {
@@ -66,13 +94,16 @@ function GrabWeaponData() {
 	console.log("Current Reload Speed: " + ReloadSpeed.toString());
 	GetFireRate();
 	console.log("Current FireRate: " + FireRate.toString());
+	console.log("\nFinished Grabing Weapon Data");
 }
+
 function BLCalculate() {
 	let secMF = parseFloat(MagSize/ FireRate);
 	let secMFR = parseFloat(secMF + ReloadSpeed);
 	let TotalMagDamage = parseFloat(MagSize * WeaponDamage);
 	let FinalDPS = parseFloat(TotalMagDamage / secMFR); 
 	x = FinalDPS.toString();
+	alert("Weapon DPS: " + x);
 	console.log("Weapon DPS: " + x);
 }
 
